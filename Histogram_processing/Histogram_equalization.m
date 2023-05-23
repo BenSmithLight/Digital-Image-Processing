050ad949c99786a304d601d1810ac1e22bd581bf
LiuGuang <1207792390@qq.com>
2023-05-23 10:44:29 +0800

    整理了直方图处理代码，删去注释的代码，添加文本注释

--- Histogram_processing/Histogram_equalization.m
+++ Histogram_processing/Histogram_equalization.m
@@ -1,4 +1,4 @@
-% 直方图均衡化函数
+% 直方图均衡化
 clc; clear; close all;
 
 %% 读取图像
@@ -6,30 +6,25 @@ image1 = imread('../Picture/test1.jpeg');
 image2 = imread('../Picture/test2.jpeg');
 image3 = imread('../Picture/test3.jpeg');
 
+% 指定处理的图像
 image = image1;
 
-%% 转换为灰度图像
+% 转换为灰度图像
 image = rgb2gray(image);
 
 %% 使用 histeq 函数进行直方图均衡化
 ienhanced_image = histeq(image);
 
 %% 显示原始图像及其直方图
-figure(1);
-subplot(2, 2, 1); imshow(image);
-title('原始图像', 'FontSize', 20);
-subplot(2, 2, 2); imhist(image);
-title('原始直方图', 'FontSize', 20);
-set(gca, 'FontSize', 14)
+figure();
+subplot(2, 2, 1); imshow(image); title('原始图像', 'FontSize', 20);
+subplot(2, 2, 2); imhist(image); title('原始直方图', 'FontSize', 20); set(gca, 'FontSize', 14)
 
-subplot(2, 2, 3); imshow(ienhanced_image);
-title('均衡化图像', 'FontSize', 20);
-subplot(2, 2, 4); imhist(ienhanced_image);
-title('均衡化直方图', 'FontSize', 20);
-set(gca, 'FontSize', 14)
+subplot(2, 2, 3); imshow(ienhanced_image); title('均衡化图像', 'FontSize', 20);
+subplot(2, 2, 4); imhist(ienhanced_image); title('均衡化直方图', 'FontSize', 20); set(gca, 'FontSize', 14)
 
 %% 显示第二张图像及其直方图
-figure(2);
+figure();
 image = image2;
 
 image = rgb2gray(image);
@@ -48,7 +43,7 @@ title('均衡化直方图', 'FontSize', 20);
 set(gca, 'FontSize', 14)
 
 %% 显示第三张图像及其直方图
-figure(3);
+figure();
 image = image3;
 
 image = rgb2gray(image);
--- Histogram_processing/Histogram_prescription.m
+++ Histogram_processing/Histogram_prescription.m
@@ -1,46 +1,45 @@
-% 直方图规定化函数
+% 直方图规定化
 clc; clear; close all;
 
-%% 读取图像
+%% 读取处理图像和目标图像
 image1 = imread('../Picture/test1.jpeg');
 image2 = imread('../Picture/test2.jpeg');
 image3 = imread('../Picture/test3.jpeg');
-
 image1_goal = imread('../Picture/test1_change.jpeg');
 image2_goal = imread('../Picture/test2_change.jpeg');
 image3_goal = imread('../Picture/test3_change.jpeg');
 
+% 指定处理图像和目标图像
 image = image3;
-
-%% 读取目标图像
 image_goal = image3_goal;
 
-%% 将原始图像和目标图像转换为灰度图像
+% 将原始图像和目标图像转换为灰度图像
 image = rgb2gray(image);
 image_goal = rgb2gray(image_goal);
 
-%% 计算原始图像和目标图像的灰度级数
+%% 直方图规定化
+% 计算原始图像和目标图像的灰度级数
 M = max(image(:)) + 1;
 N = max(image_goal(:)) + 1;
 
-%% 计算原始图像和目标图像的灰度直方图
+% 计算原始图像和目标图像的灰度直方图
 HI = imhist(image, M);
 HT = imhist(image_goal, N);
 
-%% 计算原始图像和目标图像的累积分布函数
+% 计算原始图像和目标图像的累积分布函数
 CI = cumsum(HI) / sum(HI);
 CT = cumsum(HT) / sum(HT);
 
-%% 初始化灰度映射函数
+% 初始化灰度映射函数
 map = zeros(M, 1);
 
-%% 对每个灰度级，寻找最接近的累积分布值，并记录其索引
+% 对每个灰度级，寻找最接近的累积分布值，并记录其索引
 for i = 1:M
     [~, ind] = min(abs(CI(i) - CT));
     map(i) = ind - 1;
 end
 
-%% 对原始图像进行灰度映射，得到规定化后的图像
+% 对原始图像进行灰度映射，得到规定化后的图像
 O = map(image + 1);
 
 %% 显示原始图像、目标图像和规定化后的图像
